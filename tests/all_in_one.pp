5eb247aaf8a558dcb3cd032cab7ba8de52391be7
Your Name <you@example.com>
2014-09-03 20:22:41 +0200

    NFS is working.

--- tests/all_in_one.pp
+++ tests/all_in_one.pp
@@ -1,5 +1,3 @@
-# Install cloudstack and kvm node on the same machine.
-#  For testing cloudstack.
 
 network::if::static { 'eth1':
   ensure       => 'up',
@@ -7,3 +5,64 @@ network::if::static { 'eth1':
   netmask      => '255.255.255.0',
   gateway      => '10.1.233.3',
 }
+
+
+# NTP p15-16
+# TODO Fix this later.
+#package { 'ntp':
+#  ensure => present,
+#}
+
+
+#service { 'ntpd':
+#  ensure => true,
+#  enable => true,
+#}  
+
+
+
+# === NFS, p16+
+#package { 'nfs-utils':
+# ensure => present,
+#}
+
+
+
+$szPrimaryStorage   = '/primary'
+$szSecondaryStorage = '/secondary'
+
+$szDefaultNfsOptionList = 'rw,async,no_root_squash,no_subtree_check'
+$szDefaultNfsClientList = '*'
+
+$hNfsExports = {
+ "$szPrimaryStorage" => {
+             'NfsOptionList' => "$szDefaultNfsOptionList",
+             'NfsClientList' => "$szDefaultNfsClientList",
+                                        }, 
+ "$szSecondaryStorage" => {
+             'NfsOptionList' => "$szDefaultNfsOptionList",
+             'NfsClientList' => "$szDefaultNfsClientList",
+                                        }, 
+}
+
+file { "$szPrimaryStorage":
+  ensure => directory,
+}
+
+file { "$szSecondaryStorage":
+  ensure => directory,
+}
+
+
+# filewall stuff
+#-A INPUT -s 172.16.10.0/24 -m state --state NEW -p udp --dport 111 -j ACCEPT
+#-A INPUT -s 172.16.10.0/24 -m state --state NEW -p tcp --dport
+# ...
+
+class { 'nfsserver':
+  require => File [ "$szPrimaryStorage", "$szSecondaryStorage" ],
+   hohNfsExports => $hNfsExports,
+#  NfsExport => $arNfsExports,
+}
+
+
